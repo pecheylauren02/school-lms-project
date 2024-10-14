44717f68716e493cb842788fd012d508bcda0493
pecheylauren02 <pechey.lauren02@gmail.com>
2024-10-14 16:22:18 +0000

    apply final migrations

--- myprofile/models.py
+++ myprofile/models.py
@@ -7,7 +7,7 @@ from cloudinary.models import CloudinaryField
 class StudentProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     date_of_birth = models.DateField(null=True, blank=True)
-    profile_picture = CloudinaryField('image')
+    profile_picture = CloudinaryField('image', default='media/default_picture.png')
     bio = models.TextField(blank=True)
     student_number = models.CharField(max_length=20, blank=True)
     phone_number = models.CharField(max_length=15, blank=True)
